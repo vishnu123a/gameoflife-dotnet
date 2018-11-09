58f3717326da8e9c51f160624415d2bb10cfc6e4
Callum Rigby <callum.rigby@udgroup.co.uk>
2018-11-09 15:57:52 +0000

    Setup tests.

---
+++ src/GameOfLife/Universe.cs
@@ -0,0 +1,15 @@
+﻿namespace GameOfLife
+{
+    public class Universe
+    {
+        public Universe(int[,] seed)
+        {
+            Cells = seed;
+        }
+
+        public int[,] Cells { get; }
+
+        public void Tick()
+        { }
+    }
+}
---
+++ test/GameOfLife.Tests/UniverseTests.cs
@@ -0,0 +1,35 @@
+﻿namespace GameOfLife.Tests
+{
+    using NUnit.Framework;
+
+    [TestFixture]
+    public class UniverseTests
+    {
+        [Test]
+        public void GivenALiveCell_WhenACellHasFewerThan2LiveNeighbours_TheCellDies()
+        {
+            var seed = new[,] {{0, 1, 0}, {0, 1, 0}, {0, 0, 0}};
+            var universe = new Universe(seed);
+
+            universe.Tick();
+
+            Assert.AreEqual(0, universe.Cells[0, 1]);
+            Assert.AreEqual(0, universe.Cells[1, 1]);
+        }
+
+        [Test]
+        public void GivenALiveCell_WhenACellHas2Or3LiveNeighbours_TheCellLives()
+        {
+        }
+
+        [Test]
+        public void GivenALiveCell_WhenACellHasMoreThan3LiveNeighbours_TheCellDies()
+        {
+        }
+
+        [Test]
+        public void GivenADeadCell_WhenACellHasExactly3LiveNeighbours_TheCellLives()
+        {
+        }
+    }
+}
