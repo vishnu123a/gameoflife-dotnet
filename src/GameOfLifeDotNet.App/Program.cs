832dc6465468ab7a245bb810fbc996a88a78f371
Callum Rigby <callum.rigby@udgroup.co.uk>
2018-11-12 11:28:54 +0000

    Implemented console app to display generations.

--- src/GameOfLifeDotNet.App/Program.cs
+++ src/GameOfLifeDotNet.App/Program.cs
@@ -2,11 +2,44 @@
 
 namespace GameOfLifeDotNet.App
 {
+    using GameOfLife;
+
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var seed = new[,]
+            {
+                {0, 0, 1, 0, 0, 0 }, 
+                {1, 0, 0, 1, 0, 0 }, 
+                {1, 0, 0, 1, 0, 0 }, 
+                {0, 1, 0, 0, 0, 0 }, 
+                {0, 0, 0, 0, 0, 0 }, 
+                {0, 0, 0, 0, 0, 0 }
+            };
+            var universe = new Universe(seed);
+
+            bool nextTick = true;
+            int currentGeneration = 0;
+            while (nextTick)
+            {
+                Console.WriteLine($"Current generation: {currentGeneration}");
+
+                for (int rowIndex = 0; rowIndex < universe.Cells.GetLength(0); rowIndex++)
+                {
+                    for (int columnIndex = 0; columnIndex < universe.Cells.GetLength(1); columnIndex++)
+                        Console.Write(universe.Cells[rowIndex, columnIndex] == 1 ? "* " : ". ");
+
+                    Console.WriteLine();
+                } 
+
+                Console.Write("Continue? (y/n): ");
+                nextTick = Console.ReadKey().KeyChar == 'y';
+
+                Console.Clear();
+                universe.Tick();
+                currentGeneration++;
+            }
         }
     }
 }
